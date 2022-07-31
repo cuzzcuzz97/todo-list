17359f5573eb67f1ef6d30210db9990eff83ba9a
MacBook Pro <quoccuong.ukh97@gmail.com>
2022-07-31 09:28:16 +0700

    add complete

--- tasks/models.py
+++ tasks/models.py
@@ -7,6 +7,7 @@ class TaskList(models.Model):
     title = models.CharField(max_length=100,blank=False)
     detail = models.TextField(max_length=100,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
