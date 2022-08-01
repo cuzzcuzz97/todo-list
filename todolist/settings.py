8b1a7e3bb4cdc018aa1dffef80c56bea950524ac
MacBook Pro <quoccuong.ukh97@gmail.com>
2022-08-01 10:34:56 +0700

    change database

--- todolist/settings.py
+++ todolist/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'phfdeEx4n3XFcDR6yv9m',
+        'HOST': 'containers-us-west-89.railway.app',
+        'PORT': '7677',
     }
 }
 
