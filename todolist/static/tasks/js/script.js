db52b6fd206186755f3c67ff9ac6e8d9b6a46960
MacBook Pro <quoccuong.ukh97@gmail.com>
2022-08-01 10:40:14 +0700

    nothing

---
+++ todolist/static/tasks/js/script.js
@@ -0,0 +1,11 @@
+function DeleteTask() {
+    let confirmAction = confirm("Are you sure to Delete?");
+    if (confirmAction) {
+      alert("successfully Delete");
+    } else {
+      alert("canceled");    
+      return false;
+    }
+  }
+
+document.getElementById('login').reset();
